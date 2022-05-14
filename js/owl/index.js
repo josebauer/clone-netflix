3855ffaee9817242f5f39bbef7eb790de32f47b4
josebauer <jose.h.bauer03@gmail.com>
2022-05-14 14:34:52 -0300

    Inclusão do projeto Replica Netflix

---
+++ js/owl/index.js
@@ -0,0 +1,7 @@
+function iniciarFilme(){
+    window.location = "https://www.youtube.com/watch?v=zsgPb-a8x4c";
+}
+
+function verInformacoes(){
+    window.location = "https://www.netflix.com/br/title/80204890";
+}
---
+++ js/owl/setup.js
@@ -0,0 +1,26 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    margin:10,
+    nav: false,
+    responsive:{
+        0:{
+            items:1
+        },
+        600:{
+            items:3
+        },
+        1000:{
+            items:5
+        }
+    }
+});
+
+$('header nav a[href^="#"]').on('click', function(e) {
+    e.preventDefault();
+    var id = $(this).attr('href'),
+    targetOffset = $(id).offset().top;
+      
+    $('html, body').animate({ 
+      scrollTop: targetOffset - 100
+    }, 500);
+});
